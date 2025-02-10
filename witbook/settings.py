46c763fa8b1ed41cedd6b289408b9d5aa81160ea
naariman <pticafugu@gmail.com>
2025-02-10 10:47:30 +0500

    ports changed

--- witbook/settings.py
+++ witbook/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'witbook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db/../db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
