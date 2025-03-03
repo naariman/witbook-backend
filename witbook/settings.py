db36f2bdf74ed271e60020195a50e976c444cbf0
naariman <pticafugu@gmail.com>
2025-03-04 01:25:47 +0500

    jwt fix

--- witbook/settings.py
+++ witbook/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
